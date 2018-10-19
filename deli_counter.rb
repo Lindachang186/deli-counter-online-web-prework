6372b114bbae8c2bfa60350e17e579f2d4713fe5
Linda Chang <lindachang186@gmail.com>
2018-10-19 18:39:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,8 +13,10 @@ end
 end
 
 def take_a_number(katz_deli)
-  katz_deli.push(name)
-  puts ("Welcome, #{name}. You are number #{katz_deli.size} in line.")
+  #katz deli should now be an array of ticket numbers
+  ticket = katz_deli.size + 1
+  katz_deli.push(ticket)
+  puts ("Welcome, you are ticket number #{ticket} is and you are number #{katz_deli.size} in line.")
 end
 
 def now_serving(katz_deli)
@@ -27,5 +29,6 @@ end
 
 line = []
 take_a_number(line)
-take_a_number(line)
-now_serving(line)
+#Welcome you are ticket number 1 and you are number 1 in line.
+#take_a_number(line)
+#now_serving(line)
